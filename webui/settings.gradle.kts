1076cb9307fc172446d51d0e4d656454baf4e791
SHuiTU68 <SHuiTU68@users.noreply.github.com>
2026-07-08 11:06:53 +0000

    feat: 项目介绍
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

---
+++ webui/settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "omk-webui"
